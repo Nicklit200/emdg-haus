bc2faa34018858066a9dbd373cd68bae73c7f48c
Nicklit200 <78360431+Nicklit200@users.noreply.github.com>
2026-08-20 13:14:07 +0200

    Load headline adjustments in Vite build

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import Home from "../app/page";
 import "../app/globals.css";
+import "../app/headline-adjustments.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
